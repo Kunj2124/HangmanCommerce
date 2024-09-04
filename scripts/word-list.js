4f6b377eb4c65b98211364e655192f45af30d8d9
Kunj <kunj.agarwal.243@gmail.com>
2024-09-04 12:08:14 +0530

    first commit

---
+++ scripts/word-list.js
@@ -0,0 +1,42 @@
+const wordList = [
+    {
+        word: "analyst",
+        hint: "A professional responsible for managing investments and portfolios.",
+    },
+    {
+        word: "planner",
+        hint: "A person who provides financial advice and planning services.",
+    },
+    {
+        word: "economist",
+        hint: "An expert in analyzing and predicting market trends.",
+    },
+    {
+        word: "auditor",
+        hint: "A professional who evaluates the financial health of businesses.",
+    },
+    {
+        word: "salesman",
+        hint: "A professional responsible for selling products and services.",
+    },
+    {
+        word: "accountant",
+        hint: "A person who helps manage a company’s money.",
+    },
+    {
+        word: "consultant",
+        hint: "A professional who specializes in analyzing and improving business processes.",
+    },
+    {
+        word: "merchant",
+        hint: "A person who buys and sells goods for profit.",
+    },
+    {
+        word: "compliance",
+        hint: "A person responsible for ensuring a company adheres to legal standards.",
+    },
+    {
+        word: "treasurer",
+        hint: "A professional responsible for overseeing the financial health of a company.",
+    },
+];
